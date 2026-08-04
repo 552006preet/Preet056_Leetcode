c96c350174f1bc11b3100e1d978e5e3284b85de0
Preet056 <552006preet@gmail.com>
2026-08-04 20:21:44 +0530

    Time: 160 ms (81.87%), Space: 20.7 MB (42.16%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hr(self,piles:list[int],speed:int)->int:
+        ttlhrs=0
+        for bananas in piles:
+            ttlhrs+=math.ceil(bananas/speed)
+        return ttlhrs
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n=max(piles)
+        ans=n
+        low,high=1,n
+        while low<=high:
+            mid=low+(high-low)//2
+            hrs=self.hr(piles,mid)
+            if hrs<=h:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
