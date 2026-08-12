052e95d91ffa11c94b3179fd1e68449fb934e7cb
Preet056 <552006preet@gmail.com>
2026-08-12 13:00:12 +0530

    Time: 0 ms (100%), Space: 19.5 MB (78.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -8,7 +8,7 @@ class Solution:
         while left<=right:
             mid=left+(right-left)//2
         
-            mid_value=matrix[mid//m][mid%m]
+            mid_value=matrix[mid//m][mid%m] #method to map 1-d index(mid) into 2-d matrix
         
             if mid_value==target:
                 return True
@@ -18,3 +18,16 @@ class Solution:
                 right=mid-1
         return False
                 
+# either use above or make a function of binary search and search the element as method if binary search then use that function inside SEARCH-MATRIC function and only check the condition
+
+# # def binarysrch(self,matrix:list[int],target:int)->int:
+    #     left,right=0,len(matrix)-1
+    #     while left<=right:
+    #         mid=left+(right-left)
+    #         if matrix[mid]==target:
+    #             return mid
+    #         elif matrix[mid]>target:
+    #             right=mid-1
+    #         else:
+    #             left=mid+1
+    #     return left
